1aced9543c5b25e4f29d09e3e477d78051e49d2b
Rainbough Phillips <rainbough@gmail.com>
2013-10-20 20:11:13 -0500

    add root to route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Massage::Application.routes.draw do
   resources :therapists
 
+  root "therapists#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
