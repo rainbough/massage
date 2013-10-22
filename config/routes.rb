b75a00b2b383e911d00f29fea4c241b88a09aeb3
Rainbough Phillips <rainbough@gmail.com>
2013-10-22 17:23:16 -0500

    adjust config for devise and generate views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Massage::Application.routes.draw do
 
   resources :therapists
 
-  root "therapists#index"
+  root :to => "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
